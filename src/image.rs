61acc03ab4d9cc660185aaddd155f8056a5b3cd5
CoffeeLake <45635607+AlexeyTolstopyatov@users.noreply.github.com>
2025-07-21 16:50:24 +0700

    image loader done? 	modified:   Cargo.toml 	modified:   src/image.rs 	modified:   src/main.rs

--- src/image.rs
+++ src/image.rs
@@ -1,5 +1,5 @@
 use crate::headers::{dos::{self, ImageDOSHeader}, windows::{self, *}};
-use std::io;
+use std::{io, ptr};
 use std::rc::Rc;
 // avoid the C/++ style => use pattern matching
 ///
@@ -21,36 +21,41 @@ pub enum NTHeaderType {
 impl NTHeaderType {
     pub fn get_number_of_sections(&self) -> u16 {
         match self {
-            Self::Machine32Bit(nt) => nt.nt_file_header.e_number_of_sections,
-            Self::Machine64Bit(nt) => nt.nt_file_header.e_number_of_sections
+            Self::Machine32Bit(h) => h.nt_file_header.e_number_of_sections,
+            Self::Machine64Bit(h) => h.nt_file_header.e_number_of_sections
         }
     }
     pub fn get_image_size(&self) -> u32 {
         match self {
-            Self::Machine32Bit(nt) => nt.nt_optional_header.e_size_of_image,
-            Self::Machine64Bit(nt) => nt.nt_optional_header.e_size_of_image,
+            Self::Machine32Bit(h) => h.nt_optional_header.e_size_of_image,
+            Self::Machine64Bit(h) => h.nt_optional_header.e_size_of_image,
             
         }
     }
     pub fn get_image_base(&self) -> u32 {
         match self {
-            Self::Machine32Bit(nt) => nt.nt_optional_header.e_image_base,
-            Self::Machine64Bit(nt) => nt.nt_optional_header.e_image_base
+            Self::Machine32Bit(h) => h.nt_optional_header.e_image_base,
+            Self::Machine64Bit(h) => h.nt_optional_header.e_image_base
+        }
+    }
+    pub fn get_size_of_headers(&self) -> u32 {
+        match self {
+            Self::Machine32Bit(h) => h.nt_optional_header.e_size_of_headers,
+            Self::Machine64Bit(h) => h.nt_optional_header.e_size_of_headers
         }
     }
     pub fn get_import_static_directory(&self) -> ImageDirectory {
         match self {
-            Self::Machine32Bit(nt) => nt.nt_optional_header.e_import_static_directory,
-            Self::Machine64Bit(nt) => nt.nt_optional_header.e_import_static_directory
+            Self::Machine32Bit(h) => h.nt_optional_header.e_import_static_directory,
+            Self::Machine64Bit(h) => h.nt_optional_header.e_import_static_directory
         }
     }
     pub fn get_optional_header_size(&self) -> u16 {
         match self {
-            Self::Machine32Bit(nt) => nt.nt_file_header.e_size_of_optional_header,
-            Self::Machine64Bit(nt) => nt.nt_file_header.e_size_of_optional_header
+            Self::Machine32Bit(h) => h.nt_file_header.e_size_of_optional_header,
+            Self::Machine64Bit(h) => h.nt_file_header.e_size_of_optional_header
         }
     }
-
 }
 ///
 /// Fills and defines type of PE image
@@ -59,49 +64,49 @@ impl NTHeaderType {
 /// Actually matches header type by `e_magic` field
 /// PE32/+ image field set in IMAGE_OPTIONAL_HEADER 
 /// 
-fn try_get_nt_header(image: &[u8]) -> io::Result<NTHeaderType> {
+pub fn try_get_nt_header(image: &[u8]) -> io::Result<NTHeaderType> {
     let mz_header = bytemuck::from_bytes::<ImageDOSHeader>(image
         .get(..std::mem::size_of::<ImageDOSHeader>())
-        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "stopped: invalid DOS header size"))?
+        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "Invalid DOS header size"))?
     );
 
     if mz_header.e_magic != dos::IMAGE_DOS_SIGNATURE {
-        return Err(io::Error::new(io::ErrorKind::InvalidData, "stopped: DOS part is incompat"));
+        return Err(io::Error::new(io::ErrorKind::InvalidData, "DOS part is incompat"));
     }
 
     let nt_header_offset = mz_header.e_lfanew as usize;
     let nt_magic_bytes = image
         .get(nt_header_offset..nt_header_offset + 4)
-        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "stopped: NT header out of bounds"))?;
+        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "NT header out of bounds"))?;
 
 
     if nt_magic_bytes != b"PE\0\0" {
         return Err(io::Error::new(
             io::ErrorKind::InvalidData,
-            "stopped: PE image signature not compatible"
+            "PE image signature not compatible"
         ));
     }
     let optional_magic_offset = nt_header_offset + windows::NT_OPTIONAL_HEADER_OFFSET;
     let optional_magic_bytes = image.get(optional_magic_offset..optional_magic_offset + 2)
-        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "stopped: Optional header out of bounds"))?;
+        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "Optional header out of bounds"))?;
     
     // PE32/+ sign matching
     match u16::from_le_bytes(optional_magic_bytes.try_into().unwrap()) {
         windows::IMAGE_NT_32_SIGNATURE => {
             let header = *bytemuck::from_bytes::<ImageNTHeader32>(image
                     .get(optional_magic_offset..std::mem::size_of::<ImageNTHeader32>()) 
-                    .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "stopped: PE32 header incomplete"))?
+                    .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "PE32 header incomplete"))?
             );
             Ok(NTHeaderType::Machine32Bit(header))
         },
         windows::IMAGE_NT_64_SIGNATURE => {
             let header = *bytemuck::from_bytes(image
                 .get(optional_magic_offset..std::mem::size_of::<ImageNTHeader64>())
-                .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "stopped: PE32+ header incomplete"))?
+                .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "PE32+ header incomplete"))?
             );
             Ok(NTHeaderType::Machine64Bit(header))
         }
-        magic => Err(io::Error::new(io::ErrorKind::InvalidData, format!("stopped: signature extra mismatch {:X}", magic)))
+        magic => Err(io::Error::new(io::ErrorKind::InvalidData, format!("Signature extra mismatch {:X}", magic)))
     }
 }
 ///
@@ -118,9 +123,9 @@ fn try_get_nt_header(image: &[u8]) -> io::Result<NTHeaderType> {
 /// For real: UPX has own header (in example) which points
 /// on specific structures located in file.
 /// 
-fn try_get_packed_section(
+pub fn try_get_packed_section(
     image: &[u8], 
-    nt_header: &NTHeaderType) -> io::Result<Rc::<ImageSectionHeader>> {
+    nt_header: &NTHeaderType) -> io::Result<ImageSectionHeader> {
     
     let sections_offset = 
         nt_header as *const _ as usize - image.as_ptr() as usize
@@ -132,24 +137,22 @@ fn try_get_packed_section(
         .get(sections_offset..(sections_offset + (sections_count * std::mem::size_of::<ImageSectionHeader>())))
         .ok_or(io::Error::new(
             io::ErrorKind::InvalidData,
-            "stopped: unable to find sections scope"
+            "Unable to find sections scope"
         ))?;
     
     let sections_slice = bytemuck::cast_slice::<u8, ImageSectionHeader>(sections_bytes);
 
     let no_sections_err: io::Error = io::Error::new(
         io::ErrorKind::NotFound,
-        "stopped: unable to find PE sections"
+        "Unable to find PE sections"
     );
     let result = *sections_slice
             .iter() // cast Iterator<T> instance
             .find(|h| h.s_name.starts_with(b".packed"))
             .ok_or(no_sections_err)?;
 
-    return Ok(Rc::new(result));
+    return Ok(result);
 }
-
-
 ///
 /// Tries to decompress and decrypt PE image.
 /// returns _optional_ type.
@@ -159,40 +162,30 @@ fn try_get_packed_section(
 ///  - correct sizes
 ///  - correct XOR key (raw bytes from ASCII string.. NOT CStr)
 /// 
-fn try_decrypt_image(packed_section: ImageSectionHeader, image_base: &[u8], mask: &[u8]) -> io::Result<Vec<u8>>{
-    let extraction_err = io::Error::new(
-        io::ErrorKind::InvalidData,
-        "out of bounds .packed"
-    );
-    let unpack_err = io::Error::new(
-        io::ErrorKind::InvalidData,
-        "unpacked section corrupted"
-    );
-
+pub fn try_decrypt_image(packed_section: ImageSectionHeader, image_base: &[u8], mask: &[u8]) -> io::Result<Vec<u8>>{
     let start = packed_section.s_virtual_address as usize;
-    let end = start + packed_section.m_virtual_size as usize;
+    let end = start + packed_section.s_size_of_raw_data as usize;
 
     let section_data = image_base
         .get(start..end)
-        .ok_or(extraction_err)?;
+        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "Packed section out of bounds"))?;
 
     let unpacked_size = section_data
-        .get(0..8)
-        .map(|r| bytemuck::pod_read_unaligned::<usize>(r))
-        .expect("stopped: bad unpacked size.");
-
-    let compressed_data = &section_data[8..]; // take slice from 8th element till the end.
-
+        .get(..8)
+        .and_then(|b| b.try_into().ok())
+        .map(u64::from_le_bytes)
+        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "Invalid unpacked size"))? as usize;
+    
     // warning.
-    let decompressed = miniz_oxide::inflate::decompress_to_vec_zlib(compressed_data)
-        .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e.to_string()))?; // <-- full report unpack_err instead
+    let decompressed = miniz_oxide::inflate::decompress_to_vec_zlib(image_base) // <-- compressed_data
+        .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e.to_string()))?;
 
     match decompressed.len() != unpacked_size {
         true => {
             return Err(io::Error::new(
                     io::ErrorKind::InvalidData,
                     format!(
-                        "stopped: processed sizes are different\r\n\texpected: {}\r\n\tgot{}",
+                        "Processed sizes are different\r\n\texpected: {}\r\n\tgot{}",
                         unpacked_size,
                         decompressed.len()
                     )
@@ -210,4 +203,107 @@ fn try_decrypt_image(packed_section: ImageSectionHeader, image_base: &[u8], mask
 
     return Ok(decrypted);
 
+}
+///
+/// Returns section headers slice for un/packing 
+/// procedure or unpacking procedure Error.
+/// 
+pub fn try_get_sections_slice(
+    image: &[u8],
+    nt_offset: usize,
+    num_sections: u16,
+) -> io::Result<&[ImageSectionHeader]> {
+    let sections_offset = nt_offset + std::mem::size_of::<u32>() + // nt_magic
+        std::mem::size_of::<ImageFileHeader>();
+    
+    let sections_size = num_sections as usize * std::mem::size_of::<ImageSectionHeader>();
+    
+    let sections_bytes = image.get(sections_offset..sections_offset + sections_size)
+        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "stopped: sections table out of bounds"))?;
+    
+    Ok(bytemuck::cast_slice(sections_bytes))
+}
+///
+/// Prepare, Call Win32 API for PE32/+ image
+/// 
+/// Tries allocate virtual memory for PE image
+/// and load it by module descriptor (see HMODULE from Windows API)
+/// 
+/// Marked unsafe because contains OS ABI calls.
+/// /TODO: rewrite unsafe scope with [safe_ffi] crate calls/
+/// 
+unsafe fn try_load_decompressed_image(image: &[u8]) -> io::Result<*mut u8> {
+    let dos_header = bytemuck::from_bytes::<ImageDOSHeader>(image
+            .get(0..std::mem::size_of::<ImageDOSHeader>())
+            .ok_or(io::Error::new(io::ErrorKind::InvalidData, "Unable to allocate DOS header space"))?
+    );
+    let nt_header = try_get_nt_header(image)?;
+    let image_size = nt_header.get_image_size() as usize;
+
+    // LPVOID for IA32(e) based machines same with next C-declared expression: 
+    // typedef void *LPVOID;
+
+    // alloc virtual memory
+    let lp_image_base = unsafe { // this is a LPVOID interpretation
+        winapi::um::memoryapi::VirtualAlloc(
+            std::ptr::null_mut(),
+            image_size,
+            winapi::um::winnt::MEM_COMMIT | winapi::um::winnt::MEM_RESERVE,
+            winapi::um::winnt::PAGE_EXECUTE_READWRITE,
+        )
+    };
+
+    if lp_image_base.is_null() {
+        return Err(io::Error::new(
+            io::ErrorKind::Other,
+            format!("VirtualAlloc failed: {}", unsafe { winapi::um::errhandlingapi::GetLastError() }),
+        ));
+    }
+
+    // how to alloc memory more safe
+    let image_base_slice = unsafe {
+        std::slice::from_raw_parts_mut(lp_image_base as *mut u8, image_size)
+    }; 
+    let image_base_ptr = lp_image_base as *mut u8;
+
+    //
+    // Now procedure starts to copy necessary PE image parts.
+    // 
+    
+    let header_size = nt_header.get_size_of_headers() as usize;
+    image_base_slice[..header_size].copy_from_slice(
+        image
+            .get(..header_size)
+            .ok_or(io::Error::new(io::ErrorKind::InvalidData, "stopped: invalid header size"))?,
+    );
+
+    let sections = image
+        .get(std::mem::size_of::<ImageDOSHeader>() + std::mem::size_of::<ImageNTHeader32>()..)
+        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "No sections data"));
+
+    let sections = match try_get_sections_slice(image, (dos_header.e_lfanew as usize), nt_header.get_number_of_sections()) { // what is actually nt_offset means??
+        Ok(s) => s,
+        Err(e) => return Err(e) // TODO: hide all procedure signs.
+    };
+
+    for section in sections {
+        if section.s_size_of_raw_data > 0 {
+            let src_start = section.s_pointer_to_raw_data as usize;
+            let src_end = src_start + section.s_size_of_raw_data as usize;
+            let dst_start = section.s_virtual_address as usize;
+            let dst_end = dst_start + section.s_size_of_raw_data as usize;
+
+            if dst_end > image_size || src_end > image.len() {
+                continue; // <-- skip bad sections
+            }
+
+            image_base_slice[dst_start..dst_end].copy_from_slice(
+                image
+                    .get(src_start..src_end)
+                    .ok_or(io::Error::new(io::ErrorKind::InvalidData, "Invalid section data"))?,
+            );
+        }
+    }
+
+    return Ok(image_base_ptr); // expected *mut u8 got *mut c_void.
 }
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,5 @@ mod headers;
 mod image;
 
 fn main() {
-    // Necessary parts
-    // 
-    // 1) PE image structure checkout
-    // 2) Attack Attack!
-    // 3) .packed sections checkout
-    // 4) static imports existance
+    
 }
